5d1d5af4d9eebd5c89d84c0bcc43c6faf76d6782
Free5Dev <saidsoumah@gmail.com>
2018-06-19 13:24:11 +0200

    entete et footer fonction bien

---
+++ footer.inc.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KBZ prémière plateforme de musique</title>
+    <!-- script d'injection des anciennes navigateurs -->
+    <!--[if (lt IE 9) & (!IEMobile)]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/livingston-css3-mediaqueries-js/1.0.0/css3-mediaqueries.js"></script>
+		<![endif]-->
+    <!-- lien di favicon -->
+    <link rel="icon" href="./images/kbzLogo.jpg"/>
+    <!-- cdn fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <!-- lien google font -->
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400" rel="stylesheet">
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- bootstrap min css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+    <!-- bootstrap min js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <!-- lien du fichier css -->
+    <link rel="stylesheet" href="./css/style.css"/>
+    <!-- lien script js -->
+    <script src="./js/script.js"></script>
+</head>
+<body>
+   <!-- footer -->
+   <footer class="container-fluid footer">
+       <div class="container footerContainer">
+           <div class="row footerRow">
+               <div class="col-sm-12 col-md-4 footerElement" >
+                   <h1><a href="index.php">K.B.Z</a></h1>
+                   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, nihil velit totam, consectetur assumenda labore praesentium corrupti aliquid placeat odio fugiat ullam fuga tenetur est cumque consequuntur, aspernatur enim blanditiis.</p>
+               </div>
+               <div class="col-sm-12 col-md-4 footerElement"  >
+                   <h1>Liens utiles</h1>
+                   <ul>
+                       <li><a href="http://"><i class="fas fa-tv"></i> KbzTv</a></li>
+                       <li><a href="http://"><i class="fab fa-blogger-b"></i> Blog</a></li>
+                       <li><a href="http://"><i class="fas fa-microphone-alt"></i> Talent</a></li>
+                       <li><a href="http://"><i class="fas fa-shopping-cart"></i> Shop</a></li>
+                   </ul>
+                   <ul>
+                       <li><a href="http://"><i class="fas fa-headphones"></i> Rap Usa</a></li>
+                       <li><a href="http://"><i class="fas fa-headphones"></i> Rap Fr</a></li>
+                       <li><a href="http://"><i class="fas fa-headphones"></i> Rap Afro</a></li>
+                       <li><a href="http://"><i class="fas fa-headphones"></i> Reggea Dancehall</a></li>
+                   </ul>
+               </div>
+               <div class="col-sm-12 col-md-4 footerElement"  >
+                   <h1>Contact</h1>
+                   <ul>
+                       <li><a href="mailto:kbzOfficiel@gmail.com"><i class="far fa-envelope"></i> kbzOfficiel@gmail.com</a></li>
+                       <li><a href="contact;inc.php"><i class="far fa-share-square"></i> Formulaire de Contact</a></li>
+                   </ul>
+                   <form action="" class="form-inline formNewsLetter">
+                       <input type="text" name="" id="" placeholder="NewsLetter..." class="form-control inputNewsLetter">
+                       <button class="btn btn-success btnNewsLetter">Abonnez</button>
+                    </form>
+               </div>
+               <figure class="container">
+                   <a href="" title="Facebook"><i class="fab fa-facebook"></i></a>
+                   <a href="" title="Gmail"><i class="fas fa-at"></i></a>
+                   <a href="" title="Youtube"><i class="fab fa-youtube"></i></a>
+                   <a href="" title="Twitter"><i class="fab fa-twitter"></i></a>
+                   <figcaption><i class="far fa-copyright"></i> K.B.Z-2018-Réalisé par <a href="http://www.saidsoumah.com" class="text-danger" target="_blank">SaidSoumah</a></figcaption>
+               </figure>
+           </div>
+       </div>
+   </footer>
+</body>
+</html>
---
+++ header.inc.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>KBZ prémière plateforme de musique</title>
+    <!-- script d'injection des anciennes navigateurs -->
+    <!--[if (lt IE 9) & (!IEMobile)]>
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/livingston-css3-mediaqueries-js/1.0.0/css3-mediaqueries.js"></script>
+		<![endif]-->
+    <!-- lien di favicon -->
+    <link rel="icon" href="./images/kbzLogo.jpg"/>
+    <!-- cdn fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <!-- jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- bootstrap min css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+    <!-- bootstrap min js -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <!-- lien du fichier css -->
+    <link rel="stylesheet" href="./css/style.css"/>
+    <!-- lien script js -->
+    <script src="./js/script.js"></script>
+</head>
+<body>
+    <header>
+        <div class="headerGauche">
+            <a href=""><i class="fab fa-facebook-f" title="Suivez nous sur Facebook"></i></a>
+            <a href="" ><i class="fab fa-youtube" title="Suivez nous sur Youtube"></i></a>
+            <a href=""><i class="fab fa-twitter" title="Suivez nous sur Twitter"></i></a>
+        </div>
+        <div class="headerDroite">
+            <form action="" class="form-inline justify-content-end">
+                <input type="text" name="" id="" placeholder="Ex:Rechercher" class="form-control formSearch" >
+                <button class="btn btn-dark btnSearch"><i class="fas fa-search"></i></button>
+            </form>
+        </div>
+    </header>
+</body>
+</html>
